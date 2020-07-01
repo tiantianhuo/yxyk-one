7fb28e83b16c24455c575a85f3bbacfd8b6f413e
zhao00kai-hub <610980288@qq.com>
2020-07-01 22:28:45 +0800

    yemian

--- yxyk-parent/sys-consumer/src/main/java/com/yxyk/fegin/article/ArticleFeign.java
+++ yxyk-parent/sys-consumer/src/main/java/com/yxyk/fegin/article/ArticleFeign.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(value = "article")
+@FeignClient(value = "menu")
 public interface ArticleFeign {
 
     /**
